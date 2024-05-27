f2219a30413c33d3981ab322090293f32ddbe12a
Eleazar Alfriadez <ealfriadez@gmail.com>
2024-05-27 08:38:35 -0500

    first commit

---
+++ src/test/java/pnp/edu/pe/PruebasUnitariasApplicationTests.java
@@ -0,0 +1,13 @@
+package pnp.edu.pe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebasUnitariasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
